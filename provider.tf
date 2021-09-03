851e1634b9d99adee7e6f7dbfe4c9247e68d529e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2021-09-03 19:09:52 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 # Configure the vSphere Provider
 provider "vsphere" {
   vsphere_server       = "esxi-node-1.intra.lab"
-  user                 = "root"
-  password             = "f=e5acd1808"
+  user                 = ""
+  password             = ""
   allow_unverified_ssl = true
 }
