facffeac95b97899c97c0f6660ecaf235151f79e
Mehmood <71871577+tech-v@users.noreply.github.com>
2022-11-24 12:24:46 +0500

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-# Configure the vSphere Provider
-provider "vsphere" {
-  vsphere_server       = "192.168.222.40"
-  user                 = "administrator"
-  password             = "Z@12345m@"
-  allow_unverified_ssl = true
-}
